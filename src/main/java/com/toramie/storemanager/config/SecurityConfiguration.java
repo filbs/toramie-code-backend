06df328f46ef98d5445d46613f658cb5eda8bbba
filbs <62603808+filbs@users.noreply.github.com>
2026-02-10 11:03:02 +0700

    Update SecurityConfiguration.java
    CORS config

--- src/main/java/com/toramie/storemanager/config/SecurityConfiguration.java
+++ src/main/java/com/toramie/storemanager/config/SecurityConfiguration.java
@@ -6,10 +6,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
@@ -19,7 +25,32 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/admin/**").authenticated()//admin authorization
                         .anyRequest().authenticated()
                 )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource () {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //React App connection auth
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+
+        //React App action
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        //React App headers
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        //Basic auth password allowed
+        configuration.setAllowCredentials(true);
+
+        //Rules application to URL in the app
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
     }
 }
